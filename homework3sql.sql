5aa3a427d7639a5354984ce8bd3b57735f70e64e
pjm187 <pjm187@case.edu>
2026-02-26 13:02:13 -0500

    Update homework3sql.sql

--- homework3sql.sql
+++ homework3sql.sql
@@ -1,7 +1,9 @@
+-- Remove tables if they already exist
 DROP TABLE IF EXISTS NurseNote; 
 DROP TABLE IF EXISTS NurseNoteVec;
 
--- a
+-- Part a)
+-- Create NurseNote table
 CREATE TABLE NurseNote(
 	patient_id VARCHAR(50),
 	address TEXT,
@@ -9,15 +11,18 @@ CREATE TABLE NurseNote(
 	visit_date DATE
 );
 
+-- Insert N1-N3
 INSERT INTO NurseNote(patient_id, address, note_text, visit_date) VALUES
 ('101', 'Cleveland, OH', 'child anxious after vaccination', '2025-02-12'),
 ('102', 'Akron, OH', 'persistent cough no fever', '2025-03-18'),
 ('103', 'Columbus, OH', 'vaccination follow-up mild fever', '2025-03-20');
 
+-- Show inserted rows
 SELECT * FROM NurseNote ORDER BY visit_date;
 
--- b
+-- Part b)
 
+-- Create vector table with TF-IDF weights
 CREATE TABLE NurseNoteVec(
 patient_id VARCHAR(50), 
 v1 DOUBLE PRECISION, -- TF-IDF for "vaccinations"
@@ -25,20 +30,25 @@ v2 DOUBLE PRECISION, -- TF-IDF for "anxious"
 v3 DOUBLE PRECISION -- TF-IDF for "cough"
 );
 
+-- Insert TF-IDF vectors from Q1
 INSERT INTO NurseNoteVec(patient_id, v1, v2, v3) VALUES
 ('101', 0.1761, 0.4771, 0),
 ('102', 0, 0, 0.4771),
 ('103', 0.1761, 0, 0);
 
+-- Show vector table
 SELECT * FROM NurseNoteVec;
 
--- c
+-- Part c)
+
+-- Degine query vector
 WITH query_vec AS (
     SELECT 
         CAST(0.1761 AS DOUBLE PRECISION) AS q1, 
         CAST(0.4771 AS DOUBLE PRECISION) AS q2, 
         CAST(0.0 AS DOUBLE PRECISION) AS q3
 )
+-- Compute the cosine similarity 
 SELECT n.patient_id, 
     -- The Dot Product / (Magnitude of Note * Magnitude of Query)
     (n.v1*q.q1 + n.v2*q.q2 + n.v3*q.q3) / 
@@ -49,7 +59,7 @@ ORDER BY cosine_sim DESC
 LIMIT 2;
 
 
--- d
+-- Part d)
 
 SELECT xmlelement(
 	NAME patient,
